2105393fad5f9c93b1ea0ce623729916d8298cc8
Rayka616 <130119468+Rayka616@users.noreply.github.com>
2023-06-16 07:41:13 +0800

    Add files via upload

---
+++ B11156043期末/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let menu = document.querySelector("#menu-bar");
+    let navbar = document.querySelector(".navbar");
+  
+    menu.onclick = () => {
+      menu.classList.toggle('fa-times');
+      navbar.classList.toggle('active');
+    }
+});
